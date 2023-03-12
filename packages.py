9df423dceb7cf66c8c2200a62b9c0c1ef0e7448a
elenmich <123307832+elenmich@users.noreply.github.com>
2023-03-12 15:40:25 +0100

    Create packages.py

---
+++ packages.py
@@ -0,0 +1,26 @@
+#packages
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import datetime as dt
+import re
+from wordcloud import WordCloud, STOPWORDS
+from textblob import TextBlob
+import snscrape.modules.twitter as sntwitter
+import nltk
+import sys
+import requests
+import urllib.parse
+import string
+from bs4 import BeautifulSoup
+import smtplib
+import time
+import pyowm
+from datetime import datetime, timedelta
+import urllib.request
+from urllib.request import urlopen
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import StaleElementReferenceException
+from datetime import datetime, timedelta
+import urllib.request
+from urllib.request import urlopen
